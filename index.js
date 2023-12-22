04e824fcbbc2c9fb3177ffca56569b7f25a8823f
Friiiman <friiiman94@gmail.com>
2023-12-22 23:41:17 +0300

    minor code fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const pathToFile = path.join(__dirname, 'counters.json');
 
 const app = express();
+const pathToFile = path.join(__dirname, 'counters.json');
 const data = JSON.parse(fs.readFileSync(pathToFile, 'utf-8'));
 
 app.get('/', (req, res) => {
