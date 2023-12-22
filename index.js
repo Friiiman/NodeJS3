3d0e5c9405d0fd89db4aa247488c795f765dded0
Friiiman <friiiman94@gmail.com>
2023-12-22 23:02:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path')
+const pathToFile = path.join(__dirname, 'counters.json')
+
+const app = express();
+const data = JSON.parse(fs.readFileSync(pathToFile, 'utf-8'))
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Main page</h1><a href="/about">About page</a> <p>${++data.counterMain}</p>`)
+    fs.writeFileSync(pathToFile, JSON.stringify(data))
+})
+app.get('/about', (req, res) => {
+    res.send(`<h1>About page</h1><a href="/">Main page</a> <p>${++data.counterAbout}</p>`)
+    fs.writeFileSync(pathToFile, JSON.stringify(data))
+})
+
+app.listen(3000);
